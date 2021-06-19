6ba8ae627b4c522c3af356d105041b22378ca566
jromerooo2 <75763058+jromerooo2@users.noreply.github.com>
2021-06-18 22:34:42 -0600

    Using GMap, adding methods class and initializing map with a default lat and long

--- GPSTracker/Form1.cs
+++ GPSTracker/Form1.cs
@@ -7,6 +7,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+
 
 namespace GPSTracker
 {
@@ -15,6 +20,31 @@ namespace GPSTracker
         public Form1()
         {
             InitializeComponent();
+        }
+
+        GMarkerGoogle marker;
+        GMapOverlay overlay;
+
+        //Lat
+        //13.697799719543818
+
+        //Long
+        //-89.21337247103375
+
+        double latinicial = 13.697799719543818;
+        double longInicial = -89.21337247103375;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gMapControl1.DragButton = MouseButtons.Left;
+            gMapControl1.CanDragMap = true;
+            gMapControl1.MapProvider = GMapProviders.GoogleMap;
+            gMapControl1.Position = new PointLatLng(latinicial, longInicial);
+            gMapControl1.MinZoom = 0;
+            gMapControl1.MaxZoom = 24;
+            gMapControl1.AutoScroll = true;
+            gMapControl1.Zoom = 9;
+
         }
     }
 }
---
+++ MethodsGMaps/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using GPSTracker;
+
+
+namespace MethodsGMaps
+{
+    public class Class1
+    {
+
+        GMarkerGoogle marker;
+        GMapOverlay overlay;
+
+        //Lat
+        //13.697799719543818
+
+        //Long
+        //-89.21337247103375
+
+        double ltInicial = 13.697799719543818;
+        double longInicial = -89.21337247103375;
+
+      
+
+
+    }
+}
